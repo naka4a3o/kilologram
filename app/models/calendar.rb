b5988eb060e2395c5ea692d9f03d7b877311cbec
naka4a3o <momokoro0123n@icloud.com>
2020-10-20 16:00:10 +0900

    calendarsテーブルの作成

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,2 +1,5 @@
 class Calendar < ApplicationRecord
+  belongs_to :user
+
+  validates :title, :start_time, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_10_18_052345) do
 
   create_table "calendars", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "start_time"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_calendars_on_user_id"
   end
-  
-ActiveRecord::Schema.define(version: 2020_10_18_025930) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -36,4 +35,5 @@ ActiveRecord::Schema.define(version: 2020_10_18_025930) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "calendars", "users"
 end
